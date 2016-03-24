2d96dec3ec838a76022f4dfb0c8cebf16860ba47
awkkk <stone8oy@users.noreply.github.com>
2016-03-24 19:36:13 +0800

    fix q and target_net_ update error

--- run_cpu.sh
+++ run_cpu.sh
@@ -12,7 +12,7 @@ epsilon_explore_idx=10000
 replay_memory_capacity=50000
 replay_start_size=500
 sampleStrategy="uniform"
-update_frequency=1000
+update_frequency=4
 discount_factor=0.98
 solver=./prototxt/aleSolver.prototxt
 skip_frame=3
@@ -20,10 +20,11 @@ show_frame=false
 model=null
 evaluate=false
 eval_epsilon=0.05
+target_q_freq=1000
 
 
 
-args="--gpu ${GPU} --gui ${GUI} --rom ${rom} --eval_epsilon ${eval_epsilon} --epsilon_start ${epsilon_start} --epsilon_min ${epsilon_min} --epsilon_decay ${epsilon_decay} --epsilon_explore_idx ${epsilon_explore_idx} --replay_memory_capacity ${replay_memory_capacity} --replay_start_size ${replay_start_size} --sampleStrategy ${sampleStrategy} --update_frequency ${update_frequency} --discount_factor ${discount_factor} --solver ${solver} --skip_frame ${skip_frame} --show_frame ${show_frame} --model ${model} --evaluate ${evaluate}"
+args="--gpu ${GPU} --gui ${GUI} --rom ${rom} --target_q_freq ${target_q_freq} --eval_epsilon ${eval_epsilon} --epsilon_start ${epsilon_start} --epsilon_min ${epsilon_min} --epsilon_decay ${epsilon_decay} --epsilon_explore_idx ${epsilon_explore_idx} --replay_memory_capacity ${replay_memory_capacity} --replay_start_size ${replay_start_size} --sampleStrategy ${sampleStrategy} --update_frequency ${update_frequency} --discount_factor ${discount_factor} --solver ${solver} --skip_frame ${skip_frame} --show_frame ${show_frame} --model ${model} --evaluate ${evaluate}"
 
 echo ${args}
 
